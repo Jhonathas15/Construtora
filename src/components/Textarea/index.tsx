485f3dd3af94b4fccfd64ce60b66c8c4ece43751
Jhonathas <jhonatasmar@gmail.com>
2024-11-19 13:09:31 -0300

    commit textarea

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -2,7 +2,7 @@ function Textarea() {
   return (
     <textarea
       rows={5}
-      className="block w-full sm:w-1/2 md:w-5/6 lg:w-1/2 text-sm bg-white border-2 border-black mb-2 rounded-lg"
+      className="block w-full p-2 sm:w-1/2 md:w-5/6 lg:w-1/2 text-sm bg-white border-2 border-black mb-2 rounded-lg"
     ></textarea>
   );
 }
