db799a3b743f4ea0a3bbf5dd49e478135f829d91
Jhonathas <jhonatasmar@gmail.com>
2024-11-22 13:22:23 -0300

    ja deu foi bom

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const clickPassword = () => setShowPassword(!showPassword);
+
   return (
     <main className="bg-[url('./assets/construtora.jpeg')] h-screen w-screen bg-no-repeat bg-cover">
       <div className="bg-white/60 h-screen w-screen">
@@ -13,19 +19,38 @@ function Login() {
           </section>
           <section className="flex flex-col justify-center">
             <form className="flex flex-col relative">
-              <input
-                className="border-black w-80 p-2 mb-1 rounded-lg"
-                type="text"
-                placeholder="Email cadastrado"
-              />
-              <input
-                className="relative border-black w-80 p-2 mb-3 rounded-lg"
-                type="text"
-                placeholder="Sua senha"
-              />
-              <button className="bg-black text-white w-80 mb-2 rounded-xl p-2">
-                Entrar
-              </button>
+              <div className="email">
+                <input
+                  className="border-black w-80 p-2 mb-1 rounded-lg outline-none"
+                  type="text"
+                  placeholder="Email cadastrado"
+                />
+              </div>
+              <div className="password relative">
+                <input
+                  className="relative border-black w-80 p-2 mb-3 rounded-lg outline-none"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Sua senha"
+                />
+                {showPassword ? (
+                  <IoEyeOff
+                    className="absolute top-3 right-2 cursor-pointer opacity-90"
+                    size={20}
+                    onClick={clickPassword}
+                  />
+                ) : (
+                  <IoEye
+                    className="absolute top-3 right-2 cursor-pointer opacity-90"
+                    size={20}
+                    onClick={clickPassword}
+                  />
+                )}
+              </div>
+              <div className="btn">
+                <button className="bg-black text-white w-80 mb-2 rounded-xl p-2">
+                  Entrar
+                </button>
+              </div>
             </form>
             <section className="flex justify-end">
               <a className="font-bold" href="">
